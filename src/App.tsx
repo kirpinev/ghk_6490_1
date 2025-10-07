65410295233b051c41bb6a3b27da91cef6bc24f3
Кирпинев Игорь Валерьевич <IKirpinev@alfabank.ru>
2025-10-07 14:42:22 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -91,7 +91,7 @@ export const App = () => {
 
   const submit = () => {
     window.gtag("event", "6490_get_sub", {
-      variant_name: "ghk_6490_1",
+      variant_name: "6490_1",
     });
 
     LS.setItem(LSKeys.ShowThx, true);
